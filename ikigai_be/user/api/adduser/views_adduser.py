2e7a86fedbb630a6346cf0b43a666f1216222ce9
siddharth0-0 <agrawalsid05@gmail.com>
2020-09-18 21:43:03 +0530

    commit add user

--- ikigai_be/user/api/adduser/views_adduser.py
+++ ikigai_be/user/api/adduser/views_adduser.py
@@ -8,45 +8,22 @@ import pdb
 
 class add_user(APIView):
     def post (self,request,format = 'json'):
-        
         pdb.set_trace()
-        print(request.data)
-        
-        user_serializer_var = userSerializer(data = request.data)
+
+        input_json = request.data
+        user_detail_var = {
+            'name': input_json['name'],
+            'email': input_json['email'],
+            'password': input_json['password'],
+            'privacy_id': 2,
+        }
+
+        user_serializer_var = userSerializer(data = user_detail_var)
         print(user_serializer_var.initial_data)
 
         if user_serializer_var.is_valid():
             user_serializer_var.save()
+            return Response ("Registration Succefull. Welcome To Ikigai")
 
-        return Response ("Registration Succefull. Welcome To Ikigai")
-
-        # another way of writing
-        # user_detail_var = {}
-
-        # user_detail_var['name'] = request.Post.get("name")
-        # user_detail_var['email'] = request.Post.get("email")
-        # user_detail_var['password'] = request.Post.get("password")
-        # user_detail_var['privacy_id'] = 2
-
-        # user_serializer_var = userSerializer(data = user_detail_var)
-        # print(user_serializer_var.initial_data)
-
-        # if user_serializer_var.is_valid():
-        #     user_serializer_var.save()
-
-        #     return Response ("Registration Succefull. Welcome To Ikigai")
-
-
-        # input_json = request
-        # output_json = {}
-
-        # try:
-        #     user_detail_var = dict(['name', 'email' , 'passsword', 'privacy_id'],
-        #     [input_json['name'],
-        #     input_json['email'],
-        #     input_json['password']])
-            
-        #     reg_input['added_by'] = str(reg_input['email_id'])
-        #     reg_input['last_modified_by'] = str(reg_input['email_id'])
-        #     return reg_input
+        return Response("Something Went Wrong. Kindly register Again")
 
